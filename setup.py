2f7a5978c284321718c0b6deebdae5665ee51cbe
alleyfrost <alleyfrost@qq.com>
2023-07-14 11:15:32 +0800

    changes

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(name='pyquery_xpath',
-      version='0.4',
+      version='0.6',
       description='Adding XPath support to pyquery',
       author='alleyfrost',
       author_email='alleyfrost@qq.com',
       url='https://github.com/alleyfrost/pyquery_xpath',
-      packages=find_packages('pyquery',),
+      packages=['pyquery_xpath'],
       long_description=long_description,
       long_description_content_type="text/markdown",
       license="GPLv3",
