0710e46fcc4617639633d9368dd8e35cec64dc2e
alleyfrost <alleyfrost@qq.com>
2023-07-14 10:40:02 +0800

    changes

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 # coding: utf-8
-from distutils.core import setup
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
+from setuptools import setup, find_packages
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except:
-    long_description = ""
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
 
-setup(
-    name='pyquery_xpath',
-    version='0.1',
-    description='Adding XPath support to pyquery',
-    author='alleyfrost',
-    author_email='alleyfrost@qq.com',
-    url='https://github.com/alleyfrost/pyquery_xpath',
-    packages=['pyquery_xpath'],
-    install_requires=['pyquery'],
-    keywords='pyquery xpath',
-    long_description=long_description,
-    python_requires=">=3.0"
-)
+setup(name='pyquery_xpath',
+      version='0.4',
+      description='Adding XPath support to pyquery',
+      author='alleyfrost',
+      author_email='alleyfrost@qq.com',
+      url='https://github.com/alleyfrost/pyquery_xpath',
+      packages=find_packages('pyquery',),
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      license="GPLv3",
+      python_requires='>=3.0',
+      install_requires=['pyquery'],
+      )
